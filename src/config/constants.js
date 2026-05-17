c895d7cc35f90eba20a8002b78b25bb3127dd76e
Sachin22 <sachintiwari.751858@gmail.com>
2026-05-17 14:07:23 +0530

    feat: add configuration constants for JWT, pagination, and rate limiting

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,5 +4,9 @@ module.exports = {
   PAGINATION: {
     DEFAULT_PAGE: 1,
     DEFAULT_LIMIT: 10,
+  },
+  RATE_LIMIT: {
+    WINDOW_MS: 15 * 60 * 1000, // 15 minutes
+    MAX_REQUESTS: 100 // 100 requests per window
   }
 };
